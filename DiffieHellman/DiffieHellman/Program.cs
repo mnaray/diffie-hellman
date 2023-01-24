38605c04f77227d766cc5d7ab7d426e16081eec7
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-01-24 14:06:04 +0100

    initial commit of .NET project

---
+++ DiffieHellman/DiffieHellman/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffieHellman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // gemeinsame zahlen
+            int p = 7823;
+            int g = 51;
+
+            // Alice
+            int a = 43;
+            int A = g ^ a % p;
+
+            // Bob
+            int b = 98;
+            int B = g ^ b % p;
+
+            // gemeinsamer Schlüssel
+            if ((double)(B ^ a) == (double)(A ^ b))
+            {
+                Console.WriteLine("Beide Schlüssel gleich!");
+            } else
+            {
+                Console.WriteLine("Die Schlüssel sind nicht gleich!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
